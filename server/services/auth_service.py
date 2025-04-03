c55b4c1805dc70d92d75ef4ffb61e01c52a7efb0
Thalisson Lopes <talissonlps11@gmail.com>
2025-04-03 20:15:13 -0300

    add hash senha

--- server/services/auth_service.py
+++ server/services/auth_service.py
@@ -5,6 +5,7 @@ from database.conn import SECRET_KEY
 from models.user import User
 from pydantic import BaseModel
 from fastapi import HTTPException
+from services.utils.hash_password import hash_password, verify_password  
 
 class UserCreate(BaseModel):
     username: str
@@ -14,7 +15,7 @@ class UserCreate(BaseModel):
 class UserLogin(BaseModel):  
     email: str  
     password: str 
-    
+
 def create_token(email: str) -> str:
     payload = {"email": email, "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1)}
     return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
@@ -23,7 +24,8 @@ def register_user(user: UserCreate, db: Session):
     if db.query(User).filter(User.email == user.email).first():
         raise HTTPException(status_code=400, detail="Email já cadastrado")
 
-    new_user = User(name=user.username, email=user.email, password=user.password)
+    hashed_password = hash_password(user.password)  
+    new_user = User(name=user.username, email=user.email, password=hashed_password)
     db.add(new_user)
     db.commit()
 
@@ -31,7 +33,7 @@ def register_user(user: UserCreate, db: Session):
 
 def login_user(user: UserLogin, db: Session):
     db_user = db.query(User).filter(User.email == user.email).first()
-    if not db_user or db_user.password != user.password:
+    if not db_user or not verify_password(user.password, db_user.password):  # 🔑 Verificando hash
         raise HTTPException(status_code=401, detail="Credenciais inválidas")
 
     return {"token": create_token(user.email)}
